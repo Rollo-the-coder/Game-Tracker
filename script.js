58f0b403481843e8f1c5f0e158fd955994284cef
Rollo-the-coder <erikgotta@gmail.com>
2020-11-03 09:58:34 +0700

    loops fun

---
+++ script.js
@@ -0,0 +1,16 @@
+
+// make if statement depending on which company the person works for which will then say their salary to them
+
+let wse = "36 million";
+let de = "47 million";
+let ps = "35 million";
+
+let work = de;
+
+if (work = wse) {
+    print('you will make 36 million')
+} else if (work = de) {
+    print('you will make 47 million')
+} else {
+    print('you will make 35 million')
+}
