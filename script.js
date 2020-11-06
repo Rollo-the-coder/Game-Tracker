fa3f53651906fdb149cc8ef78d20ea36aaac28a3
Rollo-the-coder <erikgotta@gmail.com>
2020-11-07 00:36:19 +0700

    games loops

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 
-// make if statement depending on which company the person works for which will then say their salary to them
+// // make if statement depending on which company the person works for which will then say their salary to them
 
-let wse = "36 million";
-let de = "47 million";
-let ps = "35 million";
+// let wse = "36 million";
+// let de = "47 million";
+// let ps = "35 million";
 
-let work = de;
+// let work = document.getElementById('input');
 
-if (work = wse) {
-    print('you will make 36 million')
-} else if (work = de) {
-    print('you will make 47 million')
-} else {
-    print('you will make 35 million')
-}
+// function logText(event) {
+//     console.log(this.textContent);
+//     return this.textContent
+// }
+
+
+// if (work = wse) {
+//     alert('you will make 36 million')
+// } else if (work = de) {
+//     alert('you will make 47 million')
+// } else {
+//     alert('you will make 35 million')
+// }
+
+
+
